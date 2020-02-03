2c64b084556d461f3d817a4631ecad22b9f192fc
dtrung98 <trung17dinhle@gmail.com>
2020-02-03 11:52:16 +0700

    effect view: fixing

--- navigationcontroller/src/main/java/com/ldt/navigation/EffectView.java
+++ navigationcontroller/src/main/java/com/ldt/navigation/EffectView.java
@@ -35,23 +35,25 @@ public interface EffectView {
     
     ViewTreeObserver getViewTreeObserver();
     
-    public EffectFrameLayout(Context context) {
+    /*
+    public EffectViee(Context context) {
         super(context);
     }
 
-    public EffectFrameLayout(Context context, AttributeSet attrs) {
+    public EffectView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public EffectFrameLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    public EffectView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public EffectFrameLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public EffectView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
+    */
 
     public void setAccordionPivotZero(float value) {
         setAlpha(1.0f);
