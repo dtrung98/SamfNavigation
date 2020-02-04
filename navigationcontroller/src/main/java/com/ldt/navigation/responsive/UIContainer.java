2fbbea1e2460cbdb9a84701f9a7c92e993bf6822
dtrung98 <trung17dinhle@gmail.com>
2020-02-04 12:23:22 +0700

    complex layout: fixing

--- navigationcontroller/src/main/java/com/ldt/navigation/responsive/UIContainer.java
+++ navigationcontroller/src/main/java/com/ldt/navigation/responsive/UIContainer.java
@@ -1,10 +1,10 @@
 package com.ldt.navigation.responsive;
 
 public interface UIContainer {
-View createLayout(int containerId, int contentId);
+void createLayout(int containerId, int contentId);
 void bind();
 void attach();
 void detach();
-default void saveState();
-default void restoreState();
+default void saveState() {}
+default void restoreState() {}
 }
