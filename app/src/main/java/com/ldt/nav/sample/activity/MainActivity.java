27c3f914540459049b3e8e2283e4c62d3ab497bc
dtrung98 <trung17dinhle@gmail.com>
2020-04-24 17:20:01 +0700

    fix crashs

--- app/src/main/java/com/ldt/nav/sample/activity/MainActivity.java
+++ app/src/main/java/com/ldt/nav/sample/activity/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity implements SplitRouter {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setDe
         setContentView(provideLayout(this));
         onCreateRouter(savedInstanceState, getSupportFragmentManager());
     }
