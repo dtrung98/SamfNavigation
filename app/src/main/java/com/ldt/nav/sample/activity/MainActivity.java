7cc57e7ae7ad2562f5d8c4008fdd8dea665da414
dtrung98 <trung17dinhle@gmail.com>
2020-09-09 17:55:47 +0700

    working: nav router fragment

--- app/src/main/java/com/ldt/nav/sample/activity/MainActivity.java
+++ app/src/main/java/com/ldt/nav/sample/activity/MainActivity.java
@@ -19,8 +19,6 @@ public class MainActivity extends AppCompatActivity implements SplitRouter {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-        getWindow().setNavigationBarColor(Color.TRANSPARENT);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         setContentView(provideLayout(this));
         onCreateRouter(savedInstanceState);
